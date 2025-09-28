ec3f2d771d021443ce4e8aaf72c35807a4480c27
Adrian <alextravc.342@gmail.com>
2025-09-27 20:53:02 -0500

    feat: CustomUserDetailsService

--- src/main/java/com/ecommerce/api/auth/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/ecommerce/api/auth/service/impl/CustomUserDetailsService.java
@@ -1,24 +1,25 @@
 package com.ecommerce.api.auth.service.impl;
 
-import com.ecommerce.api.shared.exceptions.UserNotFoundException;
 import com.ecommerce.api.users.entity.Usuario;
 import com.ecommerce.api.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 
 @Service
 @RequiredArgsConstructor
-public class CustomUserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     // @Autowired all FinalProps in auto by constructor thanks to @RequiredArgsConstructor
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UserNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Usuario user = userRepository.findByEmail(email).orElseThrow(
-                () -> new UserNotFoundException("Invalid Token")
+                () -> new UsernameNotFoundException("Invalid Token")
         );
         return new UserDetailsImpl(user);
     }
